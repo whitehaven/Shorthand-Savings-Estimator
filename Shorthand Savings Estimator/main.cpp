8c4f4e5c9405c29544560d5dbec4663f4aac8d91
Alex Crist <WhiteHaven@whitehaven-mbp.app.byu.edu>
2014-02-12 10:55:48 -0700

    Initial Commit

---
+++ Shorthand Savings Estimator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Shorthand Savings Estimator
+//
+//  Created by Alex Crist on 2/12/14.
+//  Copyright (c) 2014 WH External Projects. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
